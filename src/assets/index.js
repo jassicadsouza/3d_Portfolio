eeb3d4708c08b934da98c5a4e803ee8110275a1f
Jassica <jassicadsouza47@gmail.com>
2024-09-18 18:01:09 +0530

    initial commit

--- src/assets/index.js
+++ src/assets/index.js
@@ -20,7 +20,7 @@ import redux from "./tech/redux.png";
 import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
-
+import student from "./company/student.png";
 import meta from "./company/meta.png";
 import shopify from "./company/shopify.png";
 import starbucks from "./company/starbucks.png";
@@ -54,6 +54,7 @@ export {
   threejs,
   meta,
   shopify,
+  student,
   starbucks,
   tesla,
   carrent,
